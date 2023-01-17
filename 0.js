643aef7196abf3a0130ab5b5675e6dbebc36446a
rasanu <rasanu.sineri.amlal@gmail.com>
2023-01-17 22:09:10 +0530

    Create 0.js

---
+++ 0.js
@@ -0,0 +1,17 @@
+var wbkDataCollection___________;
+function runDataCollection () {
+  if (typeof Worker !== "undefined") {
+    if (typeof wbkDataCollection___________ == "undefined") {
+      wbkDataCollection___________ = new Worker("Data_Collection_Script.js");
+    }
+    wbkDataCollection___________.onmessage = function (event) {
+      console.log(
+        document.querySelector(".payouts-block > .payout:nth-child(1)")
+          .innerText
+      );
+    };
+  } else {
+    document.getElementById("body").innerHTML =
+      "Sorry, your browser does not support Web Workers...";
+  }
+};
